a277f08878ce0bcbec5f4de50f2ba52c08626fb5
Anthony Chu <anthony@anthonychu.ca>
2021-04-15 17:30:51 -0700

    Add project files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='swa_api',
+      version='0.1',
+      description='SWA API',
+      url='http://github.com/anthonychu/swa-api-python',
+      author='Anthony',
+      author_email='antchu@microsoft.com',
+      license='MIT',
+      packages=['swa_api'],
+      zip_safe=False)
